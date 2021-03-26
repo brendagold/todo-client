283cdb89130cf8293dd54ead6f6ebc0612a5776e
Brenda <petersbrenda47@gmail.com>
2021-03-26 18:15:49 -0400

    updated api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:8000"
+    baseURL:"https://todo-jf-server.herokuapp.com/"
+    
+    //baseURL:"http://localhost:8000"
 })
 
 export default api;
